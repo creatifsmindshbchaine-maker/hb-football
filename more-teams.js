a7c6efe8f5643ba84ef263be6f7123f57a531933
creatifsmindshbchaine-maker <creatifsmindshbchaine@gmail.com>
2026-08-15 17:36:31 +0100

    Ajouter 12 nouvelles équipes originales à HB Football

---
+++ more-teams.js
@@ -0,0 +1,16 @@
+// Extension des équipes originales de HB Football
+export const moreTeams = [
+ {name:'Casablanca Stars',color:'#f2c94c'},
+ {name:'Rabat Royals',color:'#7b61ff'},
+ {name:'Marrakech Lions',color:'#ff7a45'},
+ {name:'Tangier Waves',color:'#20c4d9'},
+ {name:'Fes Warriors',color:'#9b51e0'},
+ {name:'Agadir Eagles',color:'#27ae60'},
+ {name:'Desert FC',color:'#d98b30'},
+ {name:'Capital United',color:'#eb5757'},
+ {name:'Mountain FC',color:'#6fcf97'},
+ {name:'Thunder FC',color:'#2f80ed'},
+ {name:'Phoenix FC',color:'#f2994a'},
+ {name:'Royal Athletic',color:'#bb6bd9'}
+];
+// Chaque équipe peut ensuite recevoir son effectif, son stade et ses statistiques.
